bc3c2cbb98a8719797c097c98936706c6f8e67ff
marianajane <mariana.musm@gmail.com>
2017-04-11 11:26:39 -0500

    Tres canchas al 95%

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
-console.log("Canchas");
 
-document.addEventListener("click", canchaFut);
+var botonCanchaFut=document.getElementById("canchaFut")
+botonCanchaFut.addEventListener("click", mostrarCanchaFut);
 
-function canchaFut() {
-    document.getElementById("cancha1").style.display = "none";
+function mostrarCanchaFut() {
+    document.getElementById("cancha1").style.display = "block";
+}
+
+var botonCanchaBasquet=document.getElementById("canchaBasquet")
+botonCanchaBasquet.addEventListener("click", mostrarCanchaBasquet);
+
+function mostrarCanchaBasquet() {
+    document.getElementById("cancha2").style.display = "block";
+}
+
+var botonCanchaTenis=document.getElementById("canchaTenis")
+botonCanchaTenis.addEventListener("click", mostrarCanchaTenis);
+
+function mostrarCanchaTenis() {
+    document.getElementById("cancha3").style.display = "block";
 }
